d5001bbd511c262470e1269470ba01bf4ecad774
ZhaiYunpeng <azhaiyunpeng@163.com>
2020-05-27 09:24:47 +0800

    配置文件上传

--- auth/src/main/java/com/work/auth/AuthApplication.java
+++ auth/src/main/java/com/work/auth/AuthApplication.java
@@ -3,6 +3,9 @@ package com.work.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ZhaiYunpeng
+ */
 @SpringBootApplication
 public class AuthApplication {
 
